b0a06d12c52b21de7920c024eb22dc239f68a9af
Patryk <patryklowicki@gmail.com>
2020-06-08 21:01:44 +0200

    serve player image, summary table, front-end fixes

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ import plotly.graph_objects as go
 import pandas as pd
 from sklearn import preprocessing
 import dash
+import dash_table
 import dash_core_components as dcc
 import dash_html_components as html
 from sklearn import preprocessing
 import dash_bootstrap_components as dbc
+import os
 
 external_stylesheets = ['https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T']
 
@@ -18,49 +20,49 @@ info_columns = [('Unnamed: 2_level_0', 'Nation'),
                   ('Unnamed: 3_level_0', 'Pos'),
                   ('Unnamed: 4_level_0', 'Squad'),
                   ('Unnamed: 5_level_0', 'Comp'),
-                  ('Position', 'Pos'),
-                  ('Position', 'Alt')]
+                  # ('Position', 'Pos'),
+                  # ('Position', 'Alt')
+                ]
 
 gk_attributes = [
 
 ]
 
-df_attributes = [
-        ('Unnamed: 27_level_0', '1/3'),
-        ('Total',               'PrgDist'),
-        ('Total',               'Cmp%'),
-        ('Long', 'Cmp%'),
-        ('Vs Dribbles', 'Tkl%'),
-        ('Vs Dribbles', 'Past'),
-        ('Tackles', 'TklW'),
-]
+df_attributes = {
+      "Passes to final third" : ('Unnamed: 27_level_0', '1/3'),
+      "Progressive passes distance" : ('Total',               'PrgDist'),
+      "Pass completion %" : ('Total',               'Cmp%'),
+      "Long pass completion %" : ('Long', 'Cmp%'),
+      "Successful tackles %" : ('Vs Dribbles', 'Tkl%'),
+      "Dribbled past" : ('Vs Dribbles', 'Past'),
+      "Tackles won" : ('Tackles', 'TklW'),
+}
 
-am_attributes = [
-        ('Standard',            'Gls'),
-        ('Expected_y',          'npxG'),
-        ('Expected_y',          'npxG/Sh'),
-        ('Unnamed: 24_level_0', 'xA'),
-        ('Unnamed: 27_level_0', '1/3'),
-        ('Total',               'PrgDist'),
-        ('Total',               'Cmp%'),
-        ('Dribbles', 'Succ'),
-        ('Dribbles',            'Succ%'),
-        ('Unnamed: 28_level_0', 'Dispos'),
-        ('Vs Dribbles',         'Past')
-       ]
+am_attributes = {
+        "Goals" : ('Standard',            'Gls'),
+        "Non-penalty xG" : ('Expected_y',          'npxG'),
+        "Non-penalty xG per shot" : ('Expected_y',          'npxG/Sh'),
+        "Xa" : ('Unnamed: 24_level_0', 'xA'),
+        "Passes to final third" : ('Unnamed: 27_level_0', '1/3'),
+        "Progressive passes distance" : ('Total',               'PrgDist'),
+        "Pass completion %" : ('Total',               'Cmp%'),
+        "Successful dribbles" : ('Dribbles', 'Succ'),
+        "Successful dribbes %" : ('Dribbles',            'Succ%'),
+        "Disposesed" : ('Unnamed: 28_level_0', 'Dispos')
+       }
 
-fw_attributes = [
-        ('Standard',            'Gls'),
-        ('Per 90 Minutes', 'npxG'),
-        ('Per 90 Minutes', 'xA'),
-        ('Standard', 'SoT%'),
-        ('Touches', 'Att Pen'),
-        ('Carries', 'PrgDist'),
-        ('Unnamed: 28_level_0', 'PPA'),
-        ('Unnamed: 30_level_0', 'Prog'),
-        ('Expected_y',          'npxG/Sh'),
-        ('Unnamed: 28_level_0', 'Dispos')
-]
+fw_attributes = {
+       'Goals' : ('Standard',            'Gls'),
+       "Non-penalty xG per 90'" : ('Per 90 Minutes', 'npxG'),
+       "xA per 90'" : ('Per 90 Minutes', 'xA'),
+       'Shots on target %' : ('Standard', 'SoT%'),
+       'Attempted passes to penalty area' : ('Touches', 'Att Pen'),
+       'Progressive carries distance' : ('Carries', 'PrgDist'),
+       'Passes to penalty area' : ('Unnamed: 28_level_0', 'PPA'),
+       'Progressive passes' : ('Unnamed: 30_level_0', 'Prog'),
+       'Non-penalty xG per shot' : ('Expected_y',          'npxG/Sh'),
+       'Dispossesed' : ('Unnamed: 28_level_0', 'Dispos')
+}
 
 
 
@@ -95,10 +97,24 @@ app.layout = dbc.Container(
                         ]
                                ))]),
 
+    dbc.Row(children = [
+        dbc.Col(
+            dbc.Card([
+                dbc.CardImg(id='player_image'),
+                dbc.CardBody(dash_table.DataTable(id='summary_table'))
+            ])
+        , width=4),
+
+        dbc.Col(
+            dcc.Graph(id='polar_bar')
+               )
+    ])
 
 
 
-    dcc.Graph(id='polar_bar')]
+
+
+    ]
 )
 
 
@@ -127,35 +143,35 @@ def make_polar_chart(playername, template):
     if template == 'AM':
         print('AM')
 
-        player_raw_values   = df.loc[df.index == playername][am_attributes]
+        player_raw_values   = df.loc[df.index == playername][list(am_attributes.values())]
         template_df = df.loc[ (df.index == playername) | ( df[('Position', 'Pos')] == 'MF' ) | (df[('Position', 'Alt')] == 'MF') ]
-        normalized_to_position = normalize(template_df, am_attributes)
-        player_normalized = normalized_to_position.loc[normalized_to_position.index == playername][am_attributes]
-        columns = [str(x) for x in am_attributes]
+        normalized_to_position = normalize(template_df, list(am_attributes.values()))
+        player_normalized = normalized_to_position.loc[normalized_to_position.index == playername][list(am_attributes.values())]
+        columns = [str(x) for x in list(am_attributes.keys())]
 
     elif template == 'DF':
         print('DF')
 
-        player_raw_values = df.loc[df.index == playername][df_attributes]
+        player_raw_values = df.loc[df.index == playername][list(df_attributes.values())]
         template_df = df.loc[ (df.index == playername) | (df[('Position', 'Pos')] == 'DF') | (df[('Position', 'Alt')] == 'DF')]    #jesli jakis zawodnik jest MF to nie da sie mu sprawdzic porownania do DF bo nie ma go w tym locu - do poprawienia!!
-        normalized_to_position = normalize(template_df, df_attributes)
-        player_normalized = normalized_to_position.loc[normalized_to_position.index == playername][df_attributes]
-        columns = [str(x) for x in df_attributes]
+        normalized_to_position = normalize(template_df, list(df_attributes.values()))
+        player_normalized = normalized_to_position.loc[normalized_to_position.index == playername][list(df_attributes.values())]
+        columns = [str(x) for x in list(df_attributes.keys())]
 
     elif template == 'FW':
         print('FW')
 
-        player_raw_values = df.loc[df.index == playername][fw_attributes]
+        player_raw_values = df.loc[df.index == playername][list(fw_attributes.values())]
         template_df = df.loc[ (df.index == playername) | (df[('Position', 'Pos')] == 'FW') | (df[('Position', 'Alt')] == 'FW')]    #jesli jakis zawodnik jest MF to nie da sie mu sprawdzic porownania do DF bo nie ma go w tym locu - do poprawienia!!
-        normalized_to_position = normalize(template_df, fw_attributes)
-        player_normalized = normalized_to_position.loc[normalized_to_position.index == playername][fw_attributes]
-        columns = [str(x) for x in fw_attributes]
+        normalized_to_position = normalize(template_df, list(fw_attributes.values()))
+        player_normalized = normalized_to_position.loc[normalized_to_position.index == playername][list(fw_attributes.values())]
+        columns = [str(x) for x in list(fw_attributes.keys())]
 
     else:
         pass
 
 
-    print(player_normalized)
+    # print(player_normalized)
     player_attr_values = player_normalized.values[0].tolist()
 
     fig = go.Figure()
@@ -170,13 +186,44 @@ def make_polar_chart(playername, template):
         text= columns)
 
     fig.update_layout(
-        title=playername,
         font_size=16,
         legend_font_size=16,
         polar_angularaxis_rotation=90,
     )
 
     return fig
+
+
+@app.callback(
+    [dash.dependencies.Output('summary_table', 'data'),
+     dash.dependencies.Output('summary_table', 'columns')],
+     [dash.dependencies.Input('player_name_input', 'value')]
+)
+def build_summary_table(input):
+    player_info = df.loc[df.index == input][info_columns]
+    player_info.reset_index(inplace=True)
+    player_info.columns = ['Name', 'Nationality', 'Position', 'Club' , 'League']
+
+    player_info = player_info.transpose()
+
+    data = player_info.to_dict("records")
+    columns = [{"name": i, "id": i} for i in [str(x) for x in player_info.columns]]
+
+    return data, columns
+
+
+@app.callback(
+    dash.dependencies.Output('player_image', 'src'),
+    [dash.dependencies.Input('player_name_input', 'value')]
+)
+def get_image(input):
+    DIRPATH = os.getcwd() + '\data\images\\'
+    imgpath = ''.join([DIRPATH, input, '.png'])
+
+    namepng = '.'.join([input, 'png'])
+    output = app.get_asset_url(namepng)
+
+    return output
 
 
 if __name__ == '__main__':
